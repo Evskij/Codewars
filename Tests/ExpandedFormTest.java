84987066a51abee705e6913e621cbba1c77caef4
evskij <metalluga2004@mail.ru>
2020-06-30 17:41:43 +0300

    Initial commit

---
+++ Tests/ExpandedFormTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+// TODO: Replace examples and use TDD development by writing your own tests
+
+public class ExpandedFormTest{
+    @Test
+    public void testSomething() {
+        assertEquals("10 + 2", ExpandedForm.expandedForm(12));
+        assertEquals("40 + 2", ExpandedForm.expandedForm(42));
+        assertEquals("70000 + 300 + 4", ExpandedForm.expandedForm(70304));
+    }
+}
